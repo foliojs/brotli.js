c4052f783f9a89401dc8d833d229b9f52a7bd7f7
Yoav Weiss <yoav.weiss@shopify.com>
2024-02-27 09:11:22 +0100

    Add a timer to fix up tests

--- test/test.js
+++ test/test.js
@@ -6,7 +6,9 @@ var compress = require('../compress');
 
 describe('brotli', function() {
   describe('compress', function() {
-    it('should compress some binary data', function() {
+    it('should compress some binary data', async function() {
+      // We need this timer, because otherwise the brotli object is empty at the time we try to compress.
+      await new Promise(r=>setTimeout(r, 10));
       var data = fs.readFileSync('build/encode.js').slice(0, 1024 * 4);
       var res = brotli.compress(data);
       assert(res.length < data.length);
